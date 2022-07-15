66023d8385368c3f730c04077da5d7eed2f130ab
Neeraj <neerajgodiyal250@gmail.com>
2022-07-15 23:32:03 +0530

    dc

---
+++ client/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { TransactionProvider } from './context/TransactionContext';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactDOM.render(
+  <TransactionProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TransactionProvider>,
+  document.getElementById('root')
+)
