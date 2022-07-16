5f3b7b0e79d79d04e388040e8825db2f30897ea9
NeerajGodiyal <neerajgodiyal250@gmail.com>
2022-07-16 13:12:31 +0530

    deb

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -57,7 +57,7 @@ const TransactionCard= ({addressTo , addressFrom , timestamp , message , keyword
 
 
 const Transaction = () => {
-    const {currentAccount} = useContext(TransactionContext);
+    const {currentAccount , transactions} = useContext(TransactionContext);
     return (
         <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
             <div className="flex flex-col md:p-12 py-12 px-4">
@@ -69,7 +69,7 @@ const Transaction = () => {
                 )}
 
                 <div className="flex flex-wrap justify-center items-center mt-10">
-                    {dummyData.reverse().map((transaction , i) => (
+                    {transactions.reverse().map((transaction , i) => (
                         <TransactionCard key = {i} {...transaction} />
                     ))}
 
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -22,7 +22,7 @@ const Input = ({ placeholder, name ,type , value , handleChange }) => (
 );
 
 const Welcome = () => {
-    const { connectWallet, currentAccount, formData , sendTransaction , handleChange } = useContext(TransactionContext);
+    const { connectWallet, currentAccount, formData , sendTransaction , handleChange , isLoading } = useContext(TransactionContext);
 
     console.log(value);
 
@@ -111,7 +111,7 @@ const Welcome = () => {
                             <Input placeholder="Enter Message" name = "message" type = "text" handleChange={handleChange}/>
 
                         <div className="h-[1px] w-full bg-gray-400 my-2"/>
-                        {false ? (
+                        {isLoading ? (
                             <Loader />
                         ) : (
                             <button
